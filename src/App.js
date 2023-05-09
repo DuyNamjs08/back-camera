47d62c42272de7e597a9670aefe2a7eea6cdfde8
DESKTOP-B20OHTT\Nam <duynam11a11999@gmail.com>
2023-05-09 17:07:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
+  const [facingMode, setFacingMode] = useState('user');
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    async function getCameraStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode } });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          videoRef.current.play();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getCameraStream();
+  }, [facingMode]);
+
+  const switchCamera = () => {
+    if (facingMode === 'user') {
+      setFacingMode('environment');
+    } else {
+      setFacingMode('user');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video ref={videoRef} />
+      <button onClick={switchCamera}>Switch Camera</button>
     </div>
   );
 }
 
 export default App;
+
+;
+
